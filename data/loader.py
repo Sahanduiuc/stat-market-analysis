8666cd4d278a21226d7543fa57e31353ae754483
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2018-07-21 15:51:04 +0200

    time series processing

--- data/loader.py
+++ data/loader.py
@@ -10,12 +10,25 @@ __author__ = 'maxim'
 
 def load(filename):
   df = pd.read_csv(filename,
-                   names=['ticker', 'period', 'date', 'time', 'open', 'high', 'low', 'close', 'vol'],
+                   names=['ticker', 'period', 'date', 'time', 'open', 'high', 'low', 'close', 'volume'],
                    parse_dates={'timestamp': ['date', 'time']},
                    date_parser=lambda x: datetime.datetime.strptime(x, '%d/%m/%y %H:%M:%S'),
                    skiprows=1)
-
   return df
+
+
+def to_changes(raw):
+  changes_df = pd.DataFrame(data={
+    'timestamp': raw.timestamp,
+    'high': raw.high.pct_change(),
+    'low': raw.low.pct_change(),
+    'open': raw.open.pct_change(),
+    'close': raw.close.pct_change(),
+    'volume': raw.volume.replace({0: 1e-5}).pct_change()
+  }, columns=['timestamp', 'high', 'low', 'open', 'close', 'volume'])
+  changes_df = changes_df.set_index('timestamp')
+  changes_df = changes_df[1:]
+  return changes_df
 
 
 def main():
@@ -30,7 +43,12 @@ def main():
   close_df = df[['timestamp', 'close']]
   close_df = close_df.set_index('timestamp')
   close_df = close_df.ix['2009-01-01':]
-  close_df.plot()
+  close_df.plot(figsize=(10, 10))
+  plt.show()
+
+  changes = to_changes(df)
+  changes = changes.ix['2009-01-01':]
+  changes.plot(subplots=True, figsize=(10, 10))
   plt.show()
 
 
